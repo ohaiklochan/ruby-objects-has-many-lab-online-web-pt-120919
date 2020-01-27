cbdcdd3a9867a1a21ea6c6a541d1f7c7ad15ee21
Chloe Foronda <caforonda103@yahoo.com>
2020-01-27 23:33:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+  
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_songs(song)
+    @songs << song
+    song.artist = self
+  end
+  
+  def songs
+    @songs
+  end
+  
+end
