0f87860381cff9a2290b8e5a0b393e58d3c3a103
Chloe Foronda <caforonda103@yahoo.com>
2020-01-28 01:14:06 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+  
+  attr_accessor :author, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+end
